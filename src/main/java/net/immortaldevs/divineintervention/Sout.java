bc1bd4289d1d80e681fed93dbae0d460b5eec2c1
Solly Watkins <solly@sollyw.com>
2022-02-07 01:28:58 +1100

    We don't need these

--- src/main/java/net/immortaldevs/divineintervention/Sout.java
+++
@@ -1,7 +0,0 @@
-package net.immortaldevs.divineintervention;
-
-public final class Sout {
-    public static void sout(Object text) {
-        System.out.println(text);
-    }
-}
--- src/main/java/net/immortaldevs/divineintervention/Util.java
+++
@@ -1,9 +0,0 @@
-package net.immortaldevs.divineintervention;
-
-import java.util.Arrays;
-
-public final class Util {
-    public static Object[] widen(Object[] objects) {
-        return Arrays.copyOf(objects, objects.length + 1);
-    }
-}
